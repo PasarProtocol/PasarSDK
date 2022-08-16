b96a45307da6543c1429895db3c0dd53708d98a9
cryptodev0115 <best.dev@outlook.com>
2022-08-15 19:19:52 -0700

    :sparkles: Implement: Implement the feature to initialize the sdk

--- src/pasarCollection.ts
+++ src/pasarCollection.ts
@@ -1,5 +1,5 @@
 /**
- * This class explicitly represents the vault service subscribed by "userDid".
+ * This class is to process the functions on pasar collection.
  */
  export class pasarCollection {
 	public getFilesService(): number {
---
+++ src/utils.ts
@@ -0,0 +1,3 @@
+export namespace utils {
+    export var testNet: boolean = false;
+}
