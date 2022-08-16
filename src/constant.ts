81bd845857050004637d3a2c22b6ec32746fa165
cryptodev0115 <best.dev@outlook.com>
2022-08-16 01:49:50 -0700

    :sparkles: Implement: Implement the interface to get the all nfts

---
+++ src/constant.ts
@@ -0,0 +1,7 @@
+export namespace valuesOnTestNet {
+    export var assistURL: string = "https://testnet.assist.pasarprotocol.io";
+}
+
+export namespace valuesOnMainNet {
+    export var assistURL: string = "https://assist.pasarprotocol.io";
+}
