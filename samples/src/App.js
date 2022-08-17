199909f4226b6065759321efc97d2728349be9e7
cryptodev0115 <best.dev@outlook.com>
2022-08-16 18:39:39 -0700

    :sparkles: Implement: Implement the simple sample project

---
+++ samples/src/App.js
@@ -0,0 +1,12 @@
+import SigninEE from './SigninEE'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SigninEE/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
