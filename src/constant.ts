3b5c31a0e40ef4170ca5f6eccf3c0e45f7db1684
cryptodev0115 <best.dev@outlook.com>
2022-08-21 19:19:31 -0700

    :sparkles: Implement: Add the information related to the contract

--- src/constant.ts
+++ src/constant.ts
@@ -1,7 +1,29 @@
 export namespace valuesOnTestNet {
     export var assistURL: string = "https://testnet.assist.pasarprotocol.io";
+
+    export var elastos: any = {
+        pasarContract: '0x2652d10A5e525959F7120b56f2D7a9cD0f6ee087',
+        stickerContract: '0xed1978c53731997f4DAfBA47C9b07957Ef6F3961',
+        pasarV2Contract: '0x19088c509C390F996802B90bdc4bFe6dc3F5AAA7',
+        stickerV2Contract: '0x32496388d7c0CDdbF4e12BDc84D39B9E42ee4CB0',
+        pasarRegisterContract: '0x2b304ffC302b402785294629674A8C2b64cEF897',
+        diaTokenContract: '0x85946E4b6AB7C5c5C60A7b31415A52C0647E3272',
+
+        chainType: 1,
+    }
 }
 
 export namespace valuesOnMainNet {
     export var assistURL: string = "https://assist.pasarprotocol.io";
+
+    export var elastos: any = {
+        pasarContract: '0x02E8AD0687D583e2F6A7e5b82144025f30e26aA0',
+        stickerContract: '0x020c7303664bc88ae92cE3D380BF361E03B78B81',
+        pasarV2Contract: '0xaeA699E4dA22986eB6fa2d714F5AC737Fe93a998',
+        stickerV2Contract: '0xF63f820F4a0bC6E966D61A4b20d24916713Ebb95',
+        pasarRegisterContract: '0x3d0AD66765C319c2A1c6330C1d815608543dcc19',
+        diaTokenContract: '0x2C8010Ae4121212F836032973919E8AeC9AEaEE5',
+
+        chainType: 1,
+    }
 }
