48943f8cee20efea0e2a687d8f034bac28c8803a
cryptodev0115 <best.dev@outlook.com>
2022-08-22 05:55:12 -0700

    :sparkles: Implement: Add the ipfs on sdk

--- src/pasarCollection.ts
+++ src/pasarCollection.ts
@@ -1,8 +1,26 @@
 /**
  * This class is to process the functions on pasar collection.
  */
- export class PasarCollection {
-	public getFilesService(): number {
-		return 1;
+
+import { create } from "ipfs-http-client";
+import { utils } from "./utils";
+import { valuesOnTestNet, valuesOnMainNet } from "./constant";
+
+export class PasarCollection {
+
+	public async mintNFT(image: any, name: string, description: string) {
+		let ipfsURL;
+
+		if(utils.testNet) {
+            ipfsURL = valuesOnTestNet.urlIPFS;
+        } else {
+            ipfsURL = valuesOnMainNet.urlIPFS;
+        }
+
+		const client = create({url: ipfsURL});
+
+		let image_add = await client.add(image);
+		console.log(image_add);
+		return "success";
 	}
 }
