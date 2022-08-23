6048ebfcbb5668ad2fc8b6f903b2aebf5d54d8a6
cryptodev0115 <best.dev@outlook.com>
2022-08-23 00:40:23 -0700

    :zap: Update: Update the function for mint

--- samples/src/mintNFT.js
+++ samples/src/mintNFT.js
@@ -1,16 +1,34 @@
 import { useState } from "react";
+import { PasarCollection } from "./library/pasarSDK";
 
 const MintNFT = () => {
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
+    const [image, setImage] = useState(null);
+    const [urlImage, setUrlImage] = useState('');
 
-    const handleMint = () => {
+    const handleMint = async () => {
+        console.log(image);
         console.log(name);
         console.log(description);
+        let pasarCollection = new PasarCollection();
+
+        let result = await pasarCollection.mintNFT(image, name, description);
+        console.log(result);
+    }
+
+    const handleChangeImage = (e) => {
+        var reader = new FileReader();
+        reader.readAsDataURL(e.target.files[0]);
+        setImage(reader.result);
     }
 
     return (
         <div>
+            <div>
+                <input type="file" onChange={e => handleChangeImage(e)}/>
+
+            </div>
             <div>
                 <h3 className="sub_title">Name</h3>
                 <input value={name} onChange={(e) => setName(e.target.value)}/>
