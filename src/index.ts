521ba4ee1f7532d44d3452066e549d091683406a
cryptodev0115 <best.dev@outlook.com>
2022-08-25 02:44:03 -0700

    :zap: Update: Update the signin feature

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@
 import { mintNFT } from "./pasarCollection";
 import { getNftsOnMarketPlace } from "./getNfts"
 import { utils } from "./utils";
+import { signin, signout } from "./signin";
 
-
-const initialize = (testnet: boolean = true) => {
+const initialize = (testnet = true) => {
     utils.testNet = testnet;
 }
 
@@ -14,4 +14,6 @@ export {
     initialize,
     getNftsOnMarketPlace,
     mintNFT,
+    signin,
+    signout
 }
