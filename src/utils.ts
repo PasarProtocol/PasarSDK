6427fa5bb26ec805e41b374436bda2075840cc8f
stiartsly <stiartsly@gmail.com>
2022-10-17 20:31:41 +0800

    update createTraderMetadata in MyProfile class

--- src/utils.ts
+++ src/utils.ts
@@ -1,26 +1,3 @@
-export interface UserDidInfo {
-    "did": string,
-    "name": string,
-    "description": string
-}
-
-export interface ImageDidInfo {
-    "image": string,
-    "kind": string,
-    "size": number,
-    "thumbnail": string,
-}
-
-export interface NFTDidInfo {
-    "version": string,
-    "type": string,
-    "name": string,
-    "description": string,
-    "creator": UserDidInfo,
-    "data": ImageDidInfo,
-    "adult": boolean,
-    "properties": any,
-}
 
 export interface UserInfo {
     name:string,
