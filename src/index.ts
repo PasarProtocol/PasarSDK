92499f4fe675eca11a021edcddcd10b22bfa12d4
cryptodev0115 <best.dev@outlook.com>
2022-10-24 00:41:30 -0700

    :zap: Update: clear the code on index.js

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,14 @@
 'use strict';
 
 import { MyProfile } from "./myprofile";
-import { getListTypes, isOnAuction } from "./listtype";
 import { Category } from "./collection/category";
 import { ERCType } from "./erctype";
-import { RoyaltyRate } from "./collection/RoyaltyRate";
-import { StringIsNumber } from "./global";
 import { Profile } from "./profile";
 import { Market } from "./market";
-import { getChainTypes as _getChainTypes} from "./chaintype";
 import { SocialLinks } from "./sociallinks";
 import { AppContext } from "./appcontext";
 import { ListType } from "./listtype";
 import { Token } from "./token";
-
-let myProfileInfo, profileInfo;
-
-const getMyProfileInfo = () => {
-    if(!myProfileInfo) {
-        myProfileInfo = new MyProfile('', '', null, null, null);
-    }
-    return myProfileInfo;
-}
 
 export {
     Category,
